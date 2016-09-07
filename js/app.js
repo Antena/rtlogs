8c5e40a9b4ab84dcbeab67f4a7f950f5c7e0ed86
Gonzalo Bellver <gonzalo@antena.co>
2016-09-07 13:35:52 -0300

    Multiple terminals CV-1684

--- js/app.js
+++ js/app.js
@@ -1,16 +1,68 @@
+'use strict';
+
 $(function () {
 
-    var server = {
-        // host: '52.34.181.81',
-        host: '52.41.254.244',
-        port: '7890'
+    /**
+     * //TODO(gb): add docs
+     *
+     * @param terminalId
+     * @param terminalName
+     * @param serverHost
+     */
+    var tailLog = function(terminalId, terminalName, serverHost) {
+        var server = {
+            host: serverHost,
+            port: '7890'
+        };
+
+        // Socket
+        var socket = new WebSocket('ws://' + server.host + ':' + server.port);
+
+        // Socket open
+        socket.onopen = function() {
+            console.log('Socket open...');
+        };
+
+        // Socket close
+        socket.onclose = function() {
+            toggleTerms(terminalId);
+            $('#' + terminalId + ' .prompt').html('<span class="red">:( Nope, the connection is out. Try again later!</span>');
+        };
+
+        // Socket message
+        socket.onmessage = function(event) {
+            console.log('New message');
+
+            var lines = event.data.split(/\n/);
+
+            for (var i = 0; i < lines.length; ++i) {
+                $.trim(lines[i]) != '' && addMessage(terminalId, $.trim(lines[i]));
+            }
+
+            toggleTerms(terminalId);
+
+        };
+
+        $('#' + terminalId +' .prompt-load').typed({
+            strings: [
+                'Tailing live access logs for ' + terminalName + ' ('  + server.host + ') ^500.^1000.^1000.'
+            ],
+            typeSpeed: 0,
+            showCursor: false,
+            callback: function() {
+                toggleTerms();
+            }
+        });
     };
 
+
     /**
+     * add docs
+     *
      * @param needle
      * @param haystack
      * @param cl
-     * @returns {string|XML|void}
+     * @returns {void|string|XML}
      */
     function hl(needle, haystack, cl) {
         var reg = new RegExp(needle, 'gi');
@@ -19,9 +71,15 @@ $(function () {
         });
     }
 
-    function addMessage(msg) {
-        var msgs = $('.prompt .message');
-        var count = msgs.length;
+    /**
+     * //TODO(gb): add docs
+     *
+     * @param terminalId
+     * @param msg
+     */
+    function addMessage(terminalId, msg) {
+        var msgs = $('#' + terminalId + ' .prompt .message'),
+            count = msgs.length;
 
         msg = hl('GET|POST', msg, 'purple');
         msg = hl('HTTP\/1\.1|HTTP\/1\.0', msg, 'orange');
@@ -29,63 +87,32 @@ $(function () {
         msg = hl('\\[.*\\]', msg, 'green');
         msg = hl('(?:[0-9]{1,3}\.){3}[0-9]{1,3}', msg, 'blue');
 
-        // if (count == 20)
-        //     msgs.first().remove();
-
-        $('<div />', {
-            'class': 'message'
-        })
+        var prompt = $('#' + terminalId + ' .prompt');
+        var msgElem = $('<div />', { 'class': 'message' })
             .html(msg)
-            .appendTo($('.prompt'));
+            .appendTo(prompt);
+
+
+        var height = msgElem.height() * count;
+        prompt.animate({ scrollTop: height }, "slow");
     }
 
-    function toggleTerms() {
-        if ($('.prompt').is(':empty')) {
+    /**
+     * //TODO(gb): add docs
+     */
+    var toggleTerms = function(terminalId) {
+        var prompt = $('#' + terminalId + ' .prompt');
+        if (prompt.is(':empty')) {
             return;
         }
 
-        if ($('.prompt-load').length) {
-            $('.prompt')
-                .show();
+        if ($('#' + terminalId +' .prompt-load').length) {
+            prompt.show();
         }
-    }
-
-    $('.prompt-load').typed({
-        strings: [
-            'Tailing live access logs from ' + server.host + '^500.^1000.^1000.'
-        ],
-        typeSpeed: 0,
-        showCursor: false,
-        callback: function() {
-            toggleTerms();
-        }
-    });
-
-    // Socket
-    var socket = new WebSocket('ws://' + server.host + ':' + server.port);
-
-    // Socket open
-    socket.onopen = function(event) {
-        console.log('Socket open...');
     };
 
-    // Socket close
-    socket.onclose = function(event) {
-        toggleTerms();
-        $('.prompt').html('<span class="red">:( Nope, the connection is out. Try again later!</span>');
-    };
 
-    // Socket message
-    socket.onmessage = function(event) {
-        console.log('New message');
-
-        var lines = event.data.split(/\n/);
-
-        for (var i = 0; i < lines.length; ++i) {
-            $.trim(lines[i]) != '' && addMessage($.trim(lines[i]));
-        }
-
-        toggleTerms();
-
-    };
+    // Init terminals
+    tailLog('fhir-middleware', 'FHIR Middleware', '52.41.254.244');
+    tailLog('lab-explorer', 'Lab Explorer', '52.34.181.81');
 });
